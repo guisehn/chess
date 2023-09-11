695fa676033bdd12c3a4a4def43676d9aa65cd74
Guilherme Sehn <guilherme@hey.com>
2023-09-10 23:20:03 -0300

    add comment about strict mode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Strict mode penalizes performance by running reducers twice,
+  // where we have heavy game calculation logic
   reactStrictMode: false,
 }
 
